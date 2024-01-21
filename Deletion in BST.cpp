d3003f7e13e18b59843c2c2d712a488070c11b1a
Huzaifa Fiaz <119066287+huzaifaawan699@users.noreply.github.com>
2024-01-21 12:49:17 +0500

    Create Deletion in BST.cpp

---
+++ Deletion in BST.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node* left, *right;
+    Node(int v) : key(v), right(NULL), left(NULL) {}
+};
+
+Node* findMin(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (key < root->key) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp = findMin(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root) {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int keys[] = {50, 30, 70, 20, 40, 60, 80};
+
+    for (int key : keys) {
+        root = deleteNode(root, key);
+    }
+
+    cout << "Original BST:" << endl;
+    inorder(root);
+
+    int keyToDelete = 30;
+    root = deleteNode(root, keyToDelete);
+
+    cout << "\nBST After Deleting " << keyToDelete << ":" << endl;
+    inorder(root);
+
+    return 0;
+}
